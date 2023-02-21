6323c08b0f0cf412c4d4f3d8a5ecf4020ac55608
artdong <1029278668@qq.com>
2023-02-21 16:56:22 +0800

    site: add antd-admin

---
+++ antd-admin/precache-manifest.18b0f0cd533707de1518f5bf9a24716e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec0933e05cc0369bf70d",
+    "url": "./static/css/main.e02a0b37.chunk.css"
+  },
+  {
+    "revision": "ec0933e05cc0369bf70d",
+    "url": "./static/js/main.ec0933e0.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "27a269f7c042b38f0e7f",
+    "url": "./static/css/2.1a7cc4b8.chunk.css"
+  },
+  {
+    "revision": "27a269f7c042b38f0e7f",
+    "url": "./static/js/2.27a269f7.chunk.js"
+  },
+  {
+    "revision": "be1a2cec0f871befcbed4dd9afda0cb8",
+    "url": "./index.html"
+  }
+];
